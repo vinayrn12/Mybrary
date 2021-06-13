4c5b8183cd67834c50011642afd36baa14824aab
unknown <vinayvrn12@gmail.com>
2021-06-13 13:17:37 +0530

    Initial author setup

--- server.js
+++ server.js
@@ -4,16 +4,19 @@ if(process.env.NODE_ENV !== "production"){
 
 const express = require('express'),
       expressLayout = require('express-ejs-layouts'),
-      mongoose = require('mongoose');
+      mongoose = require('mongoose'),
+      bodyParser = require('body-parser'),
       app = express();
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/author');
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
-app.set("layout", "/layouts/layout");
+app.set("layout", "./layouts/layout");
 app.use(expressLayout);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({limit: "10mb", extended: false}));
 
 //db setup
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
@@ -24,6 +27,7 @@ db.once('open', () => console.log("Connected to mongoose"));
 
 //routes
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("The server has started");
